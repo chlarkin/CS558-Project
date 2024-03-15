7a0c75c8377f9c9a86fb07e69711498d196c1dda
chlarkin <cqlarkin@gmail.com>
2024-03-15 15:41:22 -0600

    created framework for data generation

---
+++ Milestone 1/data_gen.py
@@ -0,0 +1,9 @@
+#Initialize
+
+#Generate random congifuration
+
+#Generate obstacle locations
+
+#Check for collision
+
+#Save data in correct format for training NN (Joint angles, obstacle locations, collision_flag)
